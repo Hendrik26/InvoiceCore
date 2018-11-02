b4d3eb9f582099cb8e9477bc1357e49610e0ea40
I. R. Vobmagturs <i+r+vobmagturs@commodea.com>
2018-11-02 21:53:32 +0100

    Duplicate classes/interfaces from Buttercup.Protects for being mocked
    The original classes are sometimes marked final, so PhpSpec cannot mock
    them using Prophecy. The copies remove that restriction.

--- src/Infrastructure/AggregateHistory.php
+++ src/Infrastructure/AggregateHistory.php
@@ -1,5 +1,7 @@
-<?php
-
+<?php declare(strict_types=1);
+/**
+ * @author I. R. Vobmagturs <i+r+vobmagturs@commodea.com>
+ */
 namespace Irvobmagturs\InvoiceCore\Infrastructure;
 
 use Buttercup\Protects\CorruptAggregateHistory;
---
+++ src/Infrastructure/AggregateRoot.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author I. R. Vobmagturs <i+r+vobmagturs@commodea.com>
+ */
+
+namespace Irvobmagturs\InvoiceCore\Infrastructure;
+
+use Buttercup\Protects\RecordsEvents;
+use Buttercup\Protects\TracksChanges;
+
+interface AggregateRoot extends RecordsEvents, IsEventSourced, TracksChanges
+{
+}
