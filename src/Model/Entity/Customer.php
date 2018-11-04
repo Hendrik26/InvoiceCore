8364e3be819cfeaeffac5382d317cc7eda52c22f
Hendrik26 <hendrik.herrdler@gmail.com>
2018-11-04 22:36:32 +0100

    Introduce class InvalidCustomerAdress as Exception.

--- src/Model/Entity/Customer.php
+++ src/Model/Entity/Customer.php
@@ -8,9 +8,32 @@
 
 namespace Irvobmagturs\InvoiceCore\Model\Entity;
 
+use Buttercup\Protects\IdentifiesAggregate;
+use Buttercup\Protects\RecordsEvents;
+use Irvobmagturs\InvoiceCore\Infrastructure\AggregateHistory;
+use Irvobmagturs\InvoiceCore\Infrastructure\AggregateRoot;
+use Irvobmagturs\InvoiceCore\Infrastructure\ApplyCallsWhenMethod;
+use Irvobmagturs\InvoiceCore\Infrastructure\RecordsEventsForBusinessMethods;
+use Irvobmagturs\InvoiceCore\Model\Event\LineItemWasAppended;
+use Irvobmagturs\InvoiceCore\Model\Event\LineItemWasRemoved;
+use Irvobmagturs\InvoiceCore\Model\Exception\InvalidInvoiceId;
+use Irvobmagturs\InvoiceCore\Model\Exception\InvalidLineItemPosition;
+use Irvobmagturs\InvoiceCore\Model\Exception\InvalidLineItemTitle;
+use Irvobmagturs\InvoiceCore\Model\Id\InvoiceId;
+use Irvobmagturs\InvoiceCore\Model\ValueObject\LineItem;
+
+
 
 class Customer implements AggregateRoot
 {
+    use RecordsEventsForBusinessMethods;
+    use ApplyCallsWhenMethod;
+    /** @var InvoiceId */
+    private $aggregateId;
+    /** @var LineItem[] */
+    private $lineItems = [];
+
+
     /**
      * Invoice constructor.
      * @param InvoiceId $aggregateId
@@ -18,6 +41,28 @@ class Customer implements AggregateRoot
     public function __construct(InvoiceId $aggregateId)
     {
         $this->aggregateId = $aggregateId;
+    }
+
+    /**
+     * @param string $
+     */
+    public function changeCustomerAdress(String $customerAdress): void
+    {
+        $this->guardEmptyAddress($customerAdress);
+        $this->recordThat(new CustomerAdressWasChanged($customerAdress));
+    }
+
+
+
+    /**
+     * @param LineItem $item
+     * @throws InvalidLineItemTitle
+     */
+    private function guardEmptyAdress(String $customerAdress): void
+    {
+        if (trim($customerAdress) === "") {
+            throw new InvalidCustomerAdress();
+        }
     }
 
 }
---
+++ src/Model/Exception/InvalidCustomerAdress.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hendr
+ * Date: 04.11.2018
+ * Time: 22:23
+ */
+
+namespace Irvobmagturs\InvoiceCore\Model\Entity;
+
+
+/**
+ * Class InvalidCustomerAdress
+ * @package Irvobmagturs\InvoiceCore\Model\Exception
+ */
+
+class InvalidCustomerAdress extends InvalidArgumentException
+{
+
+    /**
+     * InvalidCustomerAdress constructor.
+     */
+    public function __construct()
+    {
+    }
+}
