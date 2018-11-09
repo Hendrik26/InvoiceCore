68d978af8309b7d2868eacd20162a68a9bf5382d
Hendrik26 <hendrik.herrdler@gmail.com>
2018-11-09 11:50:06 +0100

    Rename employDirectDebit() to employSepaDirectDebit() in Invoice.php and InvoiceSpec.php.

--- spec/Model/Entity/InvoiceSpec.php
+++ spec/Model/Entity/InvoiceSpec.php
@@ -190,7 +190,7 @@ class InvoiceSpec extends ObjectBehavior
     function it_employs_direct_debit(SepaDirectDebitMandate $mandate)
     {
         $this->clearRecordedEvents();
-        $this->employDirectDebit($mandate);
+        $this->employSepaDirectDebit($mandate);
         $recordedEvents = $this->getRecordedEvents();
         $recordedEvents->shouldHaveCount(1);
         $recordedEvents[0]->shouldBeAnInstanceOf(RecordedEvent::class);
