0f83107c0c68e0ac966c6850a08fb18552fd8328
I. R. Vobmagturs <i+r+vobmagturs@commodea.com>
2018-11-23 01:12:03 +0100

    Rewire handlers in index.php

--- index.php
+++ index.php
@@ -4,54 +4,41 @@
  * @author I. R. Vobmagturs <i+r+vobmagturs@commodea.com>
  */
 
-use Buttercup\Protects\IdentifiesAggregate;
 use GraphQL\Server\StandardServer;
 use Irvobmagturs\InvoiceCore\CommandHandler\CustomerHandler;
 use Irvobmagturs\InvoiceCore\CommandHandler\InvoiceHandler;
-use Irvobmagturs\InvoiceCore\Infrastructure\EventStore;
+use Irvobmagturs\InvoiceCore\Infrastructure\EventBus;
 use Irvobmagturs\InvoiceCore\Infrastructure\GraphQL\CqrsCommandHandlersResolver;
 use Irvobmagturs\InvoiceCore\Infrastructure\GraphQL\CustomizedGraphqlServerConfig;
-use Irvobmagturs\InvoiceCore\Infrastructure\GraphQL\EventBus;
 use Irvobmagturs\InvoiceCore\Infrastructure\GraphQL\SchemaFileCache;
 use Irvobmagturs\InvoiceCore\Infrastructure\GraphQL\TypeResolver;
-use Irvobmagturs\InvoiceCore\Infrastructure\NoEventsStored;
 use Irvobmagturs\InvoiceCore\Infrastructure\SqLiteEventStore;
+use Irvobmagturs\InvoiceCore\Infrastructure\SqLitePdo;
 use Irvobmagturs\InvoiceCore\Repository\CustomerRepository;
+use Irvobmagturs\InvoiceCore\Repository\InvoiceRepository;
 use Jubjubbird\Respects\DomainEvents;
 
 require_once __DIR__ . '/vendor/autoload.php';
 $schemaCache = __DIR__ . '/data/cache/schema';
+$eventStoreFile = __DIR__ . '/data/eventstore.sqlite';
 $schemaFile = __DIR__ . '/cqrs.graphqls';
 $context = null;
 $rootValue = null;
 $serverConfig = null;
+$eventStore = new SqLiteEventStore(new SqLitePdo($eventStoreFile));
+$eventBus = new class implements EventBus
+{
+    function dispatch(DomainEvents $domainEvents): void
+    {
+    }
+};
 $typeResolver = new TypeResolver();
 $typeResolver->addResolverForField('CqrsQuery', 'loadFoo', function () {
     return 'bar';
 });
 $typeResolver = new CqrsCommandHandlersResolver($typeResolver);
-$typeResolver = new InvoiceHandler(null, $typeResolver);
-$typeResolver = new CustomerHandler(
-    new CustomerRepository(
-        new class implements EventStore
-        {
-            public function append(array $recordedEvents): void
-            {
-            }
-
-            public function listEventsForId(IdentifiesAggregate $id): iterable
-            {
-                throw new NoEventsStored();
-            }
-        },
-        new class implements EventBus
-        {
-            function dispatch(DomainEvents $domainEvents): void
-            {
-            }
-        }
-    ), $typeResolver
-);
+$typeResolver = new InvoiceHandler(new InvoiceRepository($eventStore, $eventBus), $typeResolver);
+$typeResolver = new CustomerHandler(new CustomerRepository($eventStore, $eventBus), $typeResolver);
 try {
     $schemaFileCache = new SchemaFileCache($schemaCache);
     $schema = $schemaFileCache->loadCacheForFile($schemaFile, $typeResolver->generateTypeConfigDecorator());
