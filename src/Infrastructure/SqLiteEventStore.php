fa086041c9f87f5af66a2aa380d9e3f0a29f0065
I. R. Vobmagturs <i+r+vobmagturs@commodea.com>
2018-11-23 01:23:40 +0100

    Reformat SqLiteEventStore

--- src/Infrastructure/SqLiteEventStore.php
+++ src/Infrastructure/SqLiteEventStore.php
@@ -22,13 +22,13 @@ use function Verraes\ClassFunctions\short;
 
 class SqLiteEventStore implements EventStore
 {
-    const EVENT_NAMESPACE = 'Irvobmagturs\\InvoiceCore\\Model\\Event\\';
     const AGGREGATE_ID_NAMESPACE = 'Irvobmagturs\\InvoiceCore\\Model\\Id\\';
-    const TABLENAME = '';
-    const RESULT_COLUMN = '';
-    const QUERY_COLUMN = '';
+    const EVENT_NAMESPACE = 'Irvobmagturs\\InvoiceCore\\Model\\Event\\';
     const EXPRESSION = '';
     const JSON_FLAGS = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
+    const QUERY_COLUMN = '';
+    const RESULT_COLUMN = '';
+    const TABLENAME = '';
     private $connection;
     /** @var SqLitePdo */
     private $database;
@@ -46,6 +46,26 @@ class SqLiteEventStore implements EventStore
         $this->dbStatement = $this->createDbStatement($this->connection);
         $this->dbWriteStatement = $this->createDbWriteStatement($this->connection);
         $this->database = $database;
+    }
+
+    /**
+     * @param DomainEvent[] $recordedEvents
+     */
+    public function append(array $recordedEvents): void
+    {
+        array_walk($recordedEvents, [$this, 'writeEvent']);
+    }
+
+    /**
+     * @param IdentifiesAggregate $id
+     * @return DomainEvent[]
+     * @throws NoEventsStored when there are no events for that ID.
+     */
+    public function listEventsForId(IdentifiesAggregate $id): array
+    {
+        $dbResults = $this->getEventsFromStatement($this->dbStatement, $id);
+        $this->guardAtLeastOneEvent($dbResults);
+        return array_map([$this, 'restoreEventFromRecord'], $dbResults);
     }
 
     /**
@@ -94,26 +114,6 @@ VALUES (
 SQL;
         $statement = $connection->prepare($sql);
         return $statement;
-    }
-
-    /**
-     * @param DomainEvent[] $recordedEvents
-     */
-    public function append(array $recordedEvents): void
-    {
-        array_walk($recordedEvents, [$this, 'writeEvent']);
-    }
-
-    /**
-     * @param IdentifiesAggregate $id
-     * @return DomainEvent[]
-     * @throws NoEventsStored when there are no events for that ID.
-     */
-    public function listEventsForId(IdentifiesAggregate $id): array
-    {
-        $dbResults = $this->getEventsFromStatement($this->dbStatement, $id);
-        $this->guardAtLeastOneEvent($dbResults);
-        return array_map([$this, 'restoreEventFromRecord'], $dbResults);
     }
 
     /**
